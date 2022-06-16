9d4f014e3d46bd4ec1affb259593aea03fb2bbf6
Suresh Gopalakrishnan <suresh.gopalakrishnan@imanage.com>
2022-06-15 23:13:16 -0700

    More guess password

--- guesspasswordtests.py
+++ guesspasswordtests.py
@@ -8,11 +8,16 @@ class GuessPasswordTests(unittest.TestCase):
         target = "Hello World!" 
         guess = guesspassword.guess_password(target)
         self.assertEqual(target,guess)
-        print("-----------------------")
+        
     def test_BruceLee(self):
         target = "BruceLee2021!" 
         guess = guesspassword.guess_password(target)
         self.assertEqual(target,guess)
-        print("-----------------------")
+        
+    def test_For_I_am_fearfully_and_wonderfully_made(self):
+        target = "For I am fearfully and wonderfully made."
+        guess = guesspassword.guess_password(target)
+        self.assertEqual(target,guess)
+        
 if __name__ == '__main__':     
      unittest.main()
